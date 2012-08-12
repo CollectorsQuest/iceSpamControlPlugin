ced400429857c2834d2546f5f89521b5fa25ce4e
IvanTanev <vankata.t@gmail.com>
2012-08-12 20:58:05 +0300

    Major model update
    - move mysql enum columns to propel enum
    - make the unique index include the credential field as well
    - remove unused form/fitler classes

--- lib/model/plugin/PluginiceModelSpamControlPeer.class.php
+++ lib/model/plugin/PluginiceModelSpamControlPeer.class.php
@@ -5,5 +5,94 @@
  */
 class PluginiceModelSpamControlPeer extends BaseiceModelSpamControlPeer
 {
-  //put your code here
+
+  /**
+   * Add a new ban or throttle
+   *
+   * @param     string $field
+   * @param     string $value
+   * @param     string $credentials
+   *
+   * @return    boolean
+   * @throws    RuntimeException
+   */
+  protected static function addBanOrThrottle(
+    $type,
+    $field,
+    $value,
+    $credentials = 'read'
+  ) {
+    if (!in_array($type, array('ban', 'throttle')))
+    {
+      throw new RuntimeException(
+        'You can add either a ban or a throttle; You supplied '. $type
+      );
+    }
+
+    if (false === array_search($field, self::getValueSet(self::FIELD)))
+    {
+      throw new RuntimeException(sprintf(
+        'Unknown field "%s", supported fields are: %s',
+        $field,
+        implode(', ', self::getValueSet(self::FIELD))
+      ));
+    }
+
+    if (false === array_search($credentials, self::getValueSet(self::CREDENTIALS)))
+    {
+      throw new RuntimeException(sprintf(
+        'Unknown credential "%s", supported credentials are: %s',
+        $credentials,
+        implode(', ', self::getValueSet($colnamesself::CREDENTIALS))
+      ));
+    }
+
+    $spamControl = new iceModelSpamControl();
+    $spamControl->setField($field);
+    $spamControl->setValue($value);
+    $spamControl->setCredentials($credentials);
+    $spamControl->setIsBanned(true);
+
+    try
+    {
+      $spamControl->save();
+    }
+    catch (PropelException $e)
+    {
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
+   * Add a new ban
+   *
+   * @param     string $field
+   * @param     string $value
+   * @param     string $credentials
+   *
+   * @return    boolean
+   * @throws    RuntimeException
+   */
+  public static function addBan($field, $value, $credentials = 'read')
+  {
+    return self::addBanOrThrottle('ban', $field, $value, $credentials);
+  }
+
+  /**
+   * Add a new throttle
+   *
+   * @param     string $field
+   * @param     string $value
+   * @param     string $credentials
+   *
+   * @return    boolean
+   * @throws    RuntimeException
+   */
+  public static function addThrottle($field, $value, $credentials = 'read')
+  {
+    return self::addBanOrThrottle('throttle', $field, $value, $credentials);
+  }
+
 }
